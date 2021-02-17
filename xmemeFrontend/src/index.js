be9dc539110c6cf2dda84269117eb4e674f64927
Prashant Kumar <prashant01072000@gmail.com>
2021-02-17 15:47:38 +0530

    xmeme
    first commit

---
+++ xmemeFrontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// CSS
+import './index.css';
+
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <h1>XMEME</h1>
+      
+    </section>
+  );
+}
+
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
